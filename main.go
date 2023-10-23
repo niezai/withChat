5d6c379a77cba2e93ba004cce11935f920481a06
niezai <2624747057@qq.com>
2023-10-23 23:35:16 +0800

    init version

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Hello(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"msg": "hello world"})
+}
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/hello", Hello)
+	r.Run(":8877") // 监听并在 0.0.0.0:8080 上启动服务
+}
